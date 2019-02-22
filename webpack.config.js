02013d3ac2b0ff9eeea7fe91799aa60b7bcfce84
Nguyen Quang An <ngquangan@gmail.com>
2019-02-22 15:36:49 +0700

    initial react project

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,5 @@
-const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const config = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
-  },
-  module: {
-    rules: [
-      {
-        use: 'babel-loader',
-        test: /\.js$/
-      },
-      {
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: 'build/'
-            }
-          },
-          "css-loader"
-        ],
-        test: /\.css$/
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-  ]
+
 };
 
 module.exports = config;
