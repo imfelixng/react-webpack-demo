35b47168be3d43138c6122978f802f06033430a3
Nguyen Quang An <ngquangan@gmail.com>
2019-02-22 16:06:55 +0700

    create react project with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,29 @@
-const config = {
+const path = require('path');
 
+const config = {
+  entry: {
+    bundle: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: '/node_modules/'
+      },
+      {
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+        test: /\.css$/
+      }
+    ]
+  }
 };
 
 module.exports = config;
